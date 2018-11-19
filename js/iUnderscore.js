95f488436f01d4dc9d0ada6cadc4480836fdbe81
hubLuo <hubluo@126.com>
2018-11-19 22:18:40 +0800

    9.迭代器： 1.加强遍历 2.加强迭代函数 3.修改mixin函数，再添加实例方法同时添加静态方法。 4.添加map,times,reduce 5.添加Underscore带注释源码

--- js/iUnderscore.js
+++ js/iUnderscore.js
@@ -67,7 +67,7 @@
     _.mixin = function( obj ){
         _.each( obj, function( name ){
             //获取静态方法
-            var func = obj[name];
+            var func =_[name]=obj[name];
             //（2）.扩展原型方法
             _.prototype[name] = function(){
                 var args = [this.wrap];
@@ -91,6 +91,68 @@
     }(_);
 
     ~function(_){
+        /*迭代器*/
+        _.forEach=function(tar,cb,star,dir){
+            //增强数据源遍历:1循环控制,2循环起点控制,3循环方向和递增大小控制,4统一对象和数组遍历
+            var keys= toString.call(tar)!=="[object Array]" &&tar.length===void 0&& _.keys(tar);
+            var execute=true,cb=cb||function(){},star=star||0,dir=dir||1;
+            var length=keys?keys.length:tar.length,star=Math.min(Math.max(0,star),length-1);//star是0到length-1的值
+            var index=dir>0?star:(length-1)-star;
+            var curValue,curKey;
+            for(;execute!==false&&index>=0&&index<length;index+=dir){
+                curKey=keys?keys[index]:index;
+                curValue=tar[curKey];
+                execute=cb(curValue,curKey,tar);
+            }
+        };
+        //获取对象键
+        _.keys=function(obj){
+            //允许错误
+            var type=toString.call(obj),nativeKeys=Object.keys;
+            if( type!=="[object Object]"&& type!=="[object Function]"  ){ return [] };
+            //获取对象键
+            if(nativeKeys){
+                return nativeKeys(obj);
+            }
+            var keys=[];
+            for(var i in obj){
+                keys.push(i);
+            }
+            return keys;
+        };
+        _.iterate=function(cb, context, args){
+            var type=toString.call(cb);
+            //1.类型迭代器类型判断,执行相应操作；
+            if(cb==null){
+                //注意包括了undefined，隐式转换后它们相同。
+                return function( value ){return value;}
+            }
+            switch(type){
+                case "[object Function]":
+                    //2.优化迭代器传参和上下文
+                    return _.optimizeCb( cb, context, args);
+                    break;
+                case "[object String]":
+                    //字符串作为属性名在数据源中查询该属性名的值，并放入新数组，最后返回该数组。
+                    break;
+                case "[object Object]":
+                    //对象作为值，与数据源(对象,数组)中每项值进行比较，相同的返回true,反之为false, 并将结果放入新数组，最后返回该数组。
+                    break;
+                case "[object Array]":
+                    break;
+            }
+        };
+        _.optimizeCb=function( func, context, args){
+            //优化迭代器传参和上下文
+            //context 是否有值  上下文对象是否设置
+            var context=context == void 0?null:context;
+            return function(){
+                return func.apply(context,arguments);
+            }
+        };
+    }(_);
+
+    ~function(_){
         /*数组操作*/
         _.uniqTest=function(){
             console.log("静态方法去重测试！");
@@ -106,7 +168,53 @@
             }
             return result;
         };
-
+        _.map = function( obj, iterator, context ){
+            //生成不同功能迭代器   函数
+            var iterator = _.iterate(iterator,context);   //return function(){}
+            var result=[];
+            _.forEach(obj,function(value,i,tar){
+                result.push(iterator(value,i,tar));
+            });
+            return result;
+        }
+        /*
+         createReduce  工厂函数生成reduce
+         */
+        var createReduce = function( dir ){
+            //缩减遍历
+            var reduce = function( obj, iteratee, memo, init ){
+                var start= 0;
+                if(!init){
+                    //如果没有传memo则使用数据源第一项
+                    var keys= toString.call(obj)!=="[object Array]" &&obj.length===void 0&& _.keys(obj);
+                    var firstKey = dir>0 ? 0 : length-1;   //确定累加的方向
+                    memo = obj[keys ? keys[firstKey] : firstKey];
+                    start+=Math.abs(dir);   //1
+                };
+                _.forEach(obj,function(curValue,curKey,tar){
+                    memo = iteratee(memo, curValue, curKey, tar )
+                    console.log(memo+"@@@@")
+                },start,dir);
+                return memo;
+            }
+            //memo  最终能累加的结果     每一次累加的过程
+            return function( obj, iteratee , memo, context ){
+                //init    初始化了memo
+                var init = arguments.length>= 3;
+                return reduce( obj, _.optimizeCb(iteratee, context, 4), memo, init );
+            }
+        }
+        _.mixin({
+            times:function(n,iteratee,context){
+                var result = Array(Math.max(0,n));
+                iteratee = _.optimizeCb( iteratee, context, 1 );   //itertee
+                for(var i=0;i<n;i++){
+                    result[i]=iteratee(i);
+                }
+                return result;
+            },
+            reduce:createReduce( 1 )   //1 || -1    dir
+        });
     }(_);
     _.mixin( _ );
     return _;
